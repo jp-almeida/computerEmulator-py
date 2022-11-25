2a159373c913febb2d0f56a3e6d2fad094099525
caiofov <cfoviana@alu.ufc.br>
2022-11-25 01:19:19 -0300

    melhoria questao 1

--- questao1.asm
+++ questao1.asm
@@ -3,15 +3,24 @@ wb 0
 
 in_out ww 2022 #input e output
 cem ww 100
-qrtcents ww 400
 dois ww 2
+oito ww 8
+vintcinc ww 25
 
-#verificar se é divisivel por 400
+#verificar se é divisivel por 400 (16 * 25)
+#por 16
 main            setX in_out
-                setY qrtcents
+                andX oito #resto da divisão por 16
+                jzK div400_cont
+                goto nao_biss
+#por 25
+div400_cont     div16X #divisao por 16
+                #TODO: jzX nao_biss #menor que 16
+                setY vintcinc
                 divXY
-                jzK biss
-                goto div4
+                jzK div4
+                goto nao_biss
+
 
 #verificar se é divisivel por 4
 div4            setX in_out
@@ -19,7 +28,7 @@ div4            setX in_out
                 jzK biss
                 goto nao_biss
 
-#verificar se é divisivel por 100
+#verificar se é divisivel por 100 (4 * 25)
 div100          setX in_out
                 setY cem
                 divXY
