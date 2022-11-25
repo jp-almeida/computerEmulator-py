ce881207057faebdf237eab8d74b46a6a523f5ae
caiofov <cfoviana@alu.ufc.br>
2022-11-25 01:29:14 -0300

    melhoria questao 4

--- questao4.asm
+++ questao4.asm
@@ -1,13 +1,22 @@
 goto main
 wb 0
  
-in_out ww 100 #input e output
+in_out ww 16 #input e output
 out ww 0 #guardar input
 d ww 1 #numero que esta sendo multiplicado por ele mesmo
 
-main      setX d
-          setY d
-          goto multip
+main    setX in_out
+        andX d #resto da divisao por 2
+        jzX eh_par
+        setX d
+        setY d
+        goto multip
+
+eh_par  setX d
+        add1X
+        movX d
+        setY d
+        goto multip
 
 multip    multXY 
           movX out #guarda o valor da multiplicacao em in_out
@@ -23,13 +32,14 @@ multip    multXY
           isGreaterXY
           jzX inc #se é menor, continua
           
-          #caso contrario, diminui D (para fazer o chão da raiz quadrada)
+          #caso contrario, diminui D (para o chão da raiz)
           setX d
           sub1X
           movX in_out
           halt
 
 inc       setY d
+          add1Y
           add1Y
           movY d
           setX d
