161f6ce40c806fe16e0472838de53c79d0f6adb4
caiofov <cfoviana@alu.ufc.br>
2022-11-25 00:06:24 -0300

    atualização questao 1

--- questao1.asm
+++ questao1.asm
@@ -2,36 +2,34 @@ goto main
 wb 0
 
 in_out ww 2022 #input e output
-qtr ww 4
 cem ww 100
 qrtcents ww 400
 
 #verificar se é divisivel por 400
-main    setX in_out
-        setY qrtcents
-        divXY
-        jzK biss
-        goto div4
+main            setX in_out
+                setY qrtcents
+                divXY
+                jzK biss
+                goto div4
 
 #verificar se é divisivel por 4
-div4    setX in_out
-        setY qtr
-        divXY
-        jzK biss
-        goto nao_biss
+div4            setX in_out
+                div4X
+                jzK biss
+                goto nao_biss
 
 #verificar se é divisivel por 100
-div100  setX in_out
-        setY qtr
-        divXY
-        jzK nao_biss
-        goto biss
+div100          setX in_out
+                setY cem
+                divXY
+                jzK nao_biss
+                goto biss
 
-biss        set1X
-            goto fim
+biss            set1X
+                goto fim
 
-nao_biss    set0X
-            goto fim
+nao_biss        set0X
+                goto fim
 
-fim               movX in_out
-                  halt
+fim             movX in_out
+                halt
