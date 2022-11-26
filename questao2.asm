176974ad9c1c4a63863a586d9663c1ee6ebc8bee
caiofov <cfoviana@alu.ufc.br>
2022-11-25 23:06:46 -0300

    correção questao 2

--- questao2.asm
+++ questao2.asm
@@ -1,37 +1,23 @@
-     goto main
-     wb 0                  
+goto main
+wb 0                  
                            
-i     ww 6
-f1    ww 1
-f2    ww 1
-f3    ww 0
-d     ww 1
-a     ww 0
-b     ww 0
+in_out ww 6
+c ww 0
 
-main set x, i
-     jz x, hlt   
-     sub x, d
-     jz x, hlt   
-     sub x, d
-     jz x, dos   
-     goto func
+main setX in_out
+     jzX zero #se x for 0
+     setY in_out
+     goto loop
 
-func jz x, hlt
-     set y, f1 
-     mov y, a  
-     set y, f2 
-     add y, a  
-     mov y, f3 
-     set y, f2 
-     mov y, f1 
-     set y, f3  
-     mov y, f2 
-     sub x, d    
-	   mov y, i
-     goto func
+loop sub1Y
+     jzY end
+     movY c
+     multXY
+     setY c
+     goto loop
 
-dos  add x, d
-     mov x, i
+zero set1X
+     goto end
+
+end  movX in_out
      halt
-hlt halt
